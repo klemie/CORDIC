74cd483cb3d9bffd54bbf9505c3014e6a40f90b8
klemie <lemieuxkristopher@gmail.com>
2023-10-12 10:53:03 -0700

    project strcuture init

---
+++ CORDIC/target/debug/deps/CORDIC-44422848f22fd801.d
@@ -0,0 +1,5 @@
+C:\Users\lemie\Documents\1 - Github\CORDIC\CORDIC\target\debug\deps\CORDIC-44422848f22fd801.rmeta: src\main.rs
+
+C:\Users\lemie\Documents\1 - Github\CORDIC\CORDIC\target\debug\deps\CORDIC-44422848f22fd801.d: src\main.rs
+
+src\main.rs:
---
+++ CORDIC/target/debug/deps/CORDIC-59a42b2cd5a23fea.d
@@ -0,0 +1,5 @@
+C:\Users\lemie\Documents\1 - Github\CORDIC\CORDIC\target\debug\deps\CORDIC-59a42b2cd5a23fea.rmeta: src\main.rs
+
+C:\Users\lemie\Documents\1 - Github\CORDIC\CORDIC\target\debug\deps\CORDIC-59a42b2cd5a23fea.d: src\main.rs
+
+src\main.rs:
